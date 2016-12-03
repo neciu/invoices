3ef9911af6d29e7939a637a261021f3c8b283799
Tomasz Netczuk <netczuk.tomasz@gmail.com>
2016-12-03 15:58:43 +0100

    Full invoice data rendering

--- src/app.js
+++ src/app.js
@@ -1,6 +1,59 @@
 import yaml from 'js-yaml';
+import fs from 'fs';
+import pug from 'pug';
+import pdf from 'html-pdf';
+import {pl as inWords} from 'in-words';
+import moment from 'moment';
 
-/* eslint-disable no-console */
-console.log(yaml);
 
-console.log('Hello!');
+const invoiceData = getInvoiceData();
+const html = renderTemplate();
+
+
+function getInvoiceData() {
+  const rawInvoiceData = yaml.safeLoad(fs.readFileSync('./invoice-data/.example.yaml', 'utf8'));
+  const {netPrice, vatRate, invoiceDate, timeSpan} = rawInvoiceData;
+
+
+  const grossPrice = fixed(Number(netPrice) * (1 + Number(vatRate)));
+  const vatPrice = fixed(Number(netPrice) * Number(vatRate));
+
+  const grossPriceZlotyPartInWords = inWords(parseInt(grossPrice));
+  const grossPriceGroszPartInWords = inWords(parseInt(grossPrice * 100 % 100));
+  const grossPriceString = Number(grossPrice).toLocaleString('pl-PL', { minimumFractionDigits: 2});
+  const netPriceString = Number(netPrice).toLocaleString('pl-PL', { minimumFractionDigits: 2});
+  const vatPriceString = Number(vatPrice).toLocaleString('pl-PL', { minimumFractionDigits: 2});
+
+
+  return {
+    ...rawInvoiceData,
+    invoiceDate: moment(invoiceDate).format('YYYY-MM-DD'),
+    grossPriceString,
+    netPriceString,
+    vatPriceString,
+    vatPercentage: Number(vatRate).toLocaleString('pl-PL', { style: 'percent', minimumFractionDigits: 2}),
+    grossPriceZlotyPartInWords,
+    grossPriceGroszPartInWords,
+    dueDate: dueDate(invoiceDate, timeSpan),
+  }
+}
+
+function fixed(number) {
+  // http://www.vat.pl/interpretacje/zaokraglanie-kwot-na-fakturze-vat--groszowe-roznice-w-sumach-pozycji-faktury--20130722-9447/
+  // Widniejące na fakturze VAT kwoty winny być zaokrąglone do pełnych groszy, przy czym końcówki poniżej 0,5 grosza pomija się, a końcówki 0,5 grosza i wyższe zaokrągla się do 1 grosza.
+  return (number + 0.0005).toFixed(2);
+}
+
+function dueDate(date, timeSpan) {
+  return moment(date).add(timeSpan, 'day').format('YYYY-MM-DD');
+}
+
+function renderTemplate() {
+  return pug.renderFile('./src/template.pug', invoiceData);
+}
+
+pdf.create(html).toFile('./invoice.pdf', function (error) {
+  if (error) {
+    throw error;
+  }
+});
